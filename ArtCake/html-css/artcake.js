31e1147b9a116a924891a1e8c7896700960922ae
Alexander Fursa <gdm73078@gmail.com>
2023-09-11 18:59:01 +0200

    Add folder

--- ArtCake/html-css/artcake.js
+++ ArtCake/html-css/artcake.js
@@ -10,3 +10,14 @@
     document.getElementById("login.html").addEventListener("click", function() {
       window.location.href = "html2.html";
     });
+
+   
+fetch('/api/registration') // Замените '/api/data' на реальный URL 
+.then(response => response.json())
+.then(data => {
+  // Обработка полученных данных и вставка их в HTML-страницу
+  document.getElementById('result').textContent = data.message;
+})
+.catch(error => {
+  console.error('Произошла ошибка:', error);
+});
